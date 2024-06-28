43fb93dedf7a291881cfa6a038f044927591039d
Vishal kumar <vishal.kumar@trigvent.com>
2024-06-28 14:28:55 +0530

    init

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
+const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode:"development",
-    entry: './src/test.tsx',
+    devtool:"cheap-module-source-map",
+    entry: {
+        popup: path.resolve('src/popup/popup.tsx'),
+    },
     module: {
         rules: [
         {
@@ -11,10 +17,35 @@ module.exports = {
         },
         ]
     },
+    plugins: [
+        new CopyPlugin({
+          patterns: [
+            {
+                from: path.resolve('src/manifest.json'),
+                to: path.resolve('dist'),
+            },
+            {
+                from: path.resolve('src/static/'),
+                to: path.resolve('dist'),
+            }
+          ],
+        }),
+        ...getHtmlPlugins([
+            'popup',
+          ]),
+    ],
     resolve: {
         extensions: ['.tsx', '.ts', '.js'],
     },
     output: {
-        filename: 'index.js',  
+        filename: '[name].js',  
     },
 }
+
+function getHtmlPlugins(chunks) {
+    return chunks.map(chunk => new HtmlPlugin({
+      title: 'Trigvent WAmessanger',
+      filename: `${chunk}.html`,
+      chunks: [chunk],
+    }))
+  }
